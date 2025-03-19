e5a9cc0cc76e07d7cd3ffd62b9938e25e05652a8
Peter Collingbourne <pcc@google.com>
2025-03-18 17:03:34 -0700

    Improve handling of protobuf temporary directory.
    We currently have the following problems:
    
    - If two test processes are run concurrently, one of them will fail
      because its mkdir() will fail. This can lead to flaky tests.
    - If a test process crashes, the next run will fail for the same
      reason. We cannot delete the old temporary directory in case it is
      being used by a concurrently running test process.
    
    Fix both of these problems by adding the pid to the temporary directory
    name. The pid is unique across the system which means that:
    
    - Multiple test processes can run with guaranteed unique temporary
      directory names.
    - It is safe to delete an existing temporary directory because it can't
      interfere with an existing test process because that process would be
      using a different name.
    
    PiperOrigin-RevId: 738192768

--- src/google/protobuf/testing/googletest.cc
+++ src/google/protobuf/testing/googletest.cc
@@ -93,7 +93,12 @@ std::string TestSourceDir() {
 namespace {
 
 std::string GetTemporaryDirectoryName() {
-  std::string result = absl::StrCat(testing::TempDir(), "protobuf_tempdir");
+#ifdef _WIN32
+  DWORD pid = GetCurrentProcessId();
+#else
+  int pid = getpid();
+#endif
+  std::string result = absl::StrCat(testing::TempDir(), "protobuf_tempdir.", pid);
 #ifdef _WIN32
   // The Win32 API accepts forward slashes as a path delimiter as long as the
   // path doesn't use the "\\?\" prefix.
@@ -117,6 +122,7 @@ class TempDirDeleter {
   std::string GetTempDir() {
     if (name_.empty()) {
       name_ = GetTemporaryDirectoryName();
+      File::DeleteRecursively(name_, nullptr, nullptr);
       ABSL_CHECK(mkdir(name_.c_str(), 0777) == 0) << strerror(errno);
 
       // Stick a file in the directory that tells people what this is, in case
